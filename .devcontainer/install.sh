6438e473642f82c7ab40f1f3a3d6cac57cd03013
alexreynolds28 <alex.reynolds@attx.tech>
2024-05-14 23:21:34 -0600

    Update to remove gitmodules

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -19,5 +19,3 @@ sudo apt-get install -y asciidoctor;
 sudo apt-get install -y asciidoc;
 # Now install all of the python packages necessary to support ModelSpace
 sudo apt-get install -y python3-matplotlib python3-plotly python3-numpy python3-pandas;
-# Now pull down our git submodules
-git submodule update --init --recursive;
