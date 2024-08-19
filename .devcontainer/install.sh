0d8db3fc12ad4e50dbc821ddfeb9b66de5546787
alexreynolds28 <alex.reynolds@attx.tech>
2024-08-19 03:09:28 -0600

    Update for 24.08 ms release

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -10,8 +10,6 @@ sudo apt-get install -y gcc g++ cmake gfortran;
 sudo apt-get install -y python3-dev swig pip;
 # Install HDF5 package for logging
 sudo apt-get install -y libhdf5-serial-dev;
-# Install ogre to produce visuals + associated packages for graphics
-sudo apt-get install -y ogre-1.12;
 # Build documentation and code coverage analysis tools
 sudo apt-get install -y doxygen clang graphviz;
 sudo apt-get install -y gcovr lcov;
